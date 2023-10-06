8e5318ca94aecfd85b250b6cc342998848a560f2
Matt Vella <mcvella@gmail.com>
2023-10-06 13:35:58 -0400

    use venv

--- run.sh
+++ run.sh
@@ -1,6 +1,9 @@
-#!/bin/sh
+#!/bin/bash
 cd `dirname $0`
 
+python3 -m pip install --user virtualenv
+python3 -m venv viam-env
+source viam-env/bin/activate
 pip install --upgrade -r requirements.txt
 
 # Be sure to use `exec` so that termination signals reach the python process,
